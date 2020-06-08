cc124d5d95150a90e82b9dc10cc3ffaf826fac0a
Unknown <kkinney2@student.fitchburgstate.edu>
2020-06-08 14:34:07 -0400

    Started Scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager Instance { get; private set; }
+    public Resources Resources { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+[System.Serializable]
+public struct Resources
+{
+    public int food;
+    public int wood;
+}
